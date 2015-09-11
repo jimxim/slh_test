39ae51337754874a8a0751b96d2d3f1bd9ae45ff
jimxim <79202792@qq.com>
2015-09-11 14:00:07 +0800

    textview

--- testcase/wangLai/provider.js
+++ testcase/wangLai/provider.js
@@ -2,6 +2,7 @@
 
 function testWanLaiProviderAll() {
 	// run("厂商测试名称或手机重复", "testProviderRepeat");
+	run("厂商新增或编辑字段", "testEditProviderFields");
 }
 
 function testProviderRepeat() {
@@ -16,4 +17,37 @@ function testProviderRepeat() {
 	addProvider(name, mobile, address, price);
 	return isIn(alertMsg, "重复");
 }
-
+function testEditProviderFields() {
+	var keys = [ "name", "mobile", "address",  "remarks" ];
+	var fields = editProviderFields(keys);
+	setElementsValue(getView(), fields);
+	// debugElements(getView());
+	var showFields = editProviderFields(keys, true);
+	return checkShowFields(getView(), showFields);
+}
+function editProviderFields(keys, show) {
+	return getDTFields("editProviderField", keys, show);
+}
+function editProviderField(key, show) {
+	var e;
+	switch (key) {
+	case "name":
+		e = new DTElement("名称", "tf", 0, "a");
+		break;
+	case "mobile":
+		e = new DTElement("手机", "tf", 1, "123456789");
+		break;
+	case "address":
+		e = new DTElement("地址", "tf", 2, "地址");
+		break;
+	case "customer":
+		e = new DTElement("客户", "tf-ac", 3, "a", 1, 0);
+		break;
+	case "remarks":
+		e = new DTElement("备注", "tv", 0, "备注");
+		break;
+	default:
+		logWarn("未知key＝" + key);
+	}
+	return e;
+}
