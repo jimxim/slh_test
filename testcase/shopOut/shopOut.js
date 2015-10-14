d65b7a966e67208002d7522369db1a08272a83e0
2hangying <15068165765@139.com>
2015-10-14 16:06:27 +0800

    1

--- testcase/shopOut/shopOut.js
+++ testcase/shopOut/shopOut.js
@@ -1,9 +1,9 @@
 //ZhangY <15068165765 at 139.com> 20151013
 function testShopOutAll() {
 	// run("批量调出+", "testShopOutDecruit");
-	 run("按批次查", "testShopOutQueryBatch");
+//	 run("按批次查", "testShopOutQueryBatch");
 	// run("按明细查", "testShopOutQueryParticular");
-//	run("批量调出+", "testShopOutQueryPassword");
+	run("批量调出+", "testShopOutQueryPassword");
 }
 
 function testShopOutDecruit() {
@@ -29,7 +29,7 @@ function testShopOutDecruit() {
 	tapFirstText();
 	var ret1 = false;
 	var a = getTextFieldValue(getScrollView(), 0);
-	var b = getTextFieldValue(getScrollView(), 3);
+	var b = getTextFieldValue(getScrollView(), 3); 
 	if (a == "3035,jkk" && b == 50) {
 		ret1 = true;
 	}
@@ -40,69 +40,64 @@ function testShopOutDecruit() {
 function testShopOutQueryBatch() {
 	tapMenu("货品管理", "当前库存");
 	var keys = [ "款号" ];
-	var fields1 = shopOutDecruitFields(keys);
-	changeTFieldValue(fields1["款号"], "3035");
-	query(fields1);
+	var fields = shopOutDecruitFields(keys);
+	changeTFieldValue(fields["款号"], "3035");
+	query(fields);
 	var qr = getQR();
-	var a1 = qr.data[0]["库存数"];
+	var a1 = qr.data[0]["库存"];
 	var a2 = qr.data[0]["在途数"];
-	debugQResult(qr);
 	
 	tapMenu("门店调出", "批量调出+");
 	var f0 = new TField("货品", TF_AC, 0, "3035", -1, 0);
 	var f3 = new TField("数量", TF, 3, "2");
-	var fields = [ f0, f3 ];
-	setTFieldsValue(getScrollView(), fields);
+	var fields1 = [ f0, f3 ];
+	setTFieldsValue(getScrollView(), fields1);
 	delay();
 
-	var keys = [ "调出人*", "接收店*" ];
-	var fields1 = shopOutDecruitFields(keys);
-	changeTFieldValue(fields1["调出人*"], "3001", -1, 0);
-	changeTFieldValue(fields1["接收店*"], "常青店(test)36新");
-	setTFieldsValue(window, fields1);
+	var keys1 = [ "调出人*", "接收店*" ];
+	var fields2 = shopOutDecruitFields(keys1);
+	changeTFieldValue(fields2["调出人*"], "3001", -1, 0);
+	changeTFieldValue(fields2["接收店*"], "常青店(test)36新");
+	setTFieldsValue(window, fields2);
 	saveAndAlertOk();
 	tapPrompt();
 	delay();
 	tapButton(window, RETURN);
 	
 	tapMenu("货品管理", "当前库存");
-	// delay();
-	// tapButton(window, QUERY);
-	query(fields1);
+	query(fields);
 	delay();
 	qr = getQR();
-	debugQResult(qr);
 	var ret1 = false;
-	var a3 = qr.data[0]["库存数"];
+	var a3 = qr.data[0]["库存"];
 	var a4 = qr.data[0]["在途数"];
 	if (sub(a3, a1) == 0 && sub(a4, a2) == 2) {
 		ret1 = true;
 	}
 	
 	tapMenu("门店调出", "按批次查");
-	var keys = [ "日期从" ];
-	var fields = shopOutQueryBatchFields(keys);
-	changeTFieldValue(fields["日期从"], "2014-08-18");
-	query(fields);
+	var keys2= [ "日期从" ];
+	var fields3 = shopOutQueryBatchFields(keys2);
+	changeTFieldValue(fields3["日期从"], "2014-08-18");
+	query(fields3);
 	tapFirstText();
 	tapButtonAndAlert("作 废");
 	delay();
 	tapButton(window, RETURN);
 	
 	tapMenu("货品管理", "当前库存");
-	// delay();
-	// tapButton(window, QUERY);
-	query(fields1);
+	query(fields);
 	delay();
 	qr = getQR();
-	debugQResult(qr);
-	var ret1 = false;
-	var a5 = qr.data[0]["库存数"];
+	var ret2 = false;
+	var a5 = qr.data[0]["库存"];
 	var a6 = qr.data[0]["在途数"];	
 	if (sub(a5, a3) == 0 && sub(a6, a4) == -2) {
-		ret1 = true;
+		ret2 = true;
 	}
 	
+	tapMenu("门店调出", "按批次查");
+	query(fields3);
 	var ret = true;
 	// ret = ret && sortByTitle("厂商");
 	// ret = ret && sortByTitle("仓库/门店");
@@ -116,7 +111,7 @@ function testShopOutQueryBatch() {
 	// ret = ret && sortByTitle("上架天数", IS_NUM);
 	// ret = ret && sortByTitle("累计销", IS_NUM);
 
-	return ret;
+	return ret1 && ret2 && ret;
 }
 
 function testShopOutQueryParticular() {
@@ -159,7 +154,7 @@ function testShopOutQueryPassword() {
 
 	saveAndAlertOk();
 	tapPrompt();
-//	tapButton(window,SAVE);
+	tapButton(window,SAVE);
 	
 	 var ret1 = false, ret2 = false;
 	 var ret3 = false, ret4 = false;
@@ -187,21 +182,24 @@ function testShopOutQueryPassword() {
 	tf1.setValue("000000");	
 	tapButton(window,SAVE);
 	saveAndAlertOk();
-	if (isIn(alertMsg, "保存成功，是否打印")) {
+	if (isIn(alertMsg, "保存成功，是否打印?")) {
 		ret5 = true;
 	}	
 	saveAndAlertOk();
 	
-	tapButton(getWindows(),"打印");
+	tapButton(window,"打 印");
 	if (isIn(alertMsg, "填写密码后才能打印")) {
 		ret6 = true;
 	}	
 	var tf2 = window.secureTextFields()[1].secureTextFields()[0];
 	tf2.setValue("110000");
-	tapButton(getWindows(),"打印");
+	tapButton(window,"打 印");
 	if (isIn(alertMsg, "操作失败,［密码错误］")) {
 		ret7 = true;
-	}		
+	}
+	saveAndAlertOk();
+	tapNaviLeftButton();
+	tapButton(window, RETURN);
 	logDebug(" ret1=" + ret1 + " ret2=" + ret2 + " ret3=" + ret3 + 
 			" ret4="	+ ret4 + " ret5=" + ret5 + " ret6="	+ ret6);
 	return ret1 && ret2 && ret3 && ret4 && ret5 && ret6 && ret7;
