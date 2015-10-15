261e3df6dcd2935260285a2a9a2146b054605eb2
jimxim <79202792@qq.com>
2015-10-15 16:19:54 +0800

    1015

--- suite.js
+++ suite.js
@@ -20,61 +20,38 @@
 #import "testcase/purchase/purchaseFields.js"
 #import "testcase/purchase/purchase.js"
 
-//总经理
+// 总经理
 function test000All() {
 	debug = true;
-//	var ok = login("000","000000");
-//	if( ok ) {
-	testSalesBillAll();
-//	testStatisticAnalysisAll();
-//	testTemp();
-//	testPurchaseAll();
-//		testParamAll();	
-//		testWanLaiCustomerAll();
-//	run("积分查询", "testQueryCustomerScoreFields");
-//	run("客户新增", "testEditCustomerFields");
-//		testWanLaiProviderAll();
-//		testGoodsAll();
-//	testGoodsGoodsAll();
-//		logout();
-//	}
+// var ok = login("000","000000");
+// if( ok ) {
+// testSalesBillAll();
+// testStatisticAnalysisAll();
+	testTemp();
+// testPurchaseAll();
+// testParamAll();
+// testWanLaiCustomerAll();
+// run("积分查询", "testQueryCustomerScoreFields");
+// run("客户新增", "testEditCustomerFields");
+// testWanLaiProviderAll();
+// testGoodsAll();
+// testGoodsGoodsAll();
+// logout();
+// }
 }
 
 function debugElements() {
-	//debugAddGoods();
+	// debugAddGoods();
 	debugGlobalParamPopValue();
 }
 
 function testTemp() {
-//	tapMenu("货品管理","货品查询");
-//	debugElementTree(getScrollView());
-//	debugElementTree(window);
-//	tapChoose(getScrollView(), [0,1,2]);
-//	tapButton(window,ALL);
-//	tapButton(window,NONE);
-//	getTimestamp(0);
-//	getTimestamp(5);
-//	getTimestamp(11);
-//	getTimestamp(13);
-//	getTimestamp(15);
-//	var key = [ "品牌", "吊牌价", "季节", "厂商", "备注" ];
-//	var fields1 = editGoodsFields(key, false, 0);
-//	setTFieldsValue(getScrollView(), fields1);
-//    tap(window.buttons['查 询']);
-//    tapButton(window, '查 询');
-//    logDebug("ok="+isUIAButton(window.buttons()['查 询']));
-//    logDebug("ok="+isUIAButton(window.buttons()["查 询"]));
-//    debugElementTree(window);
-//    var qr = getQResult();
-//    debugQResult(qr);
-//    var keys = [ "店员" ];
-//    var fields = purchaseBatchEditFields(keys);
-//    setTFieldsValue(window, fields);
-    scrollNextPage();
-    delay();
-    var qr = getQResult2(getScrollView(1), "操作日期", "累计未结");
-debugQResult(qr);
-//    debugElementTree(getScrollView(1));
-var texts = getStaticTexts(getScrollView(1));
-debugArray(texts);
+  var ok = login("000","000000");
+  if( ok ) {
+      logout();
+  }
+  ok = login("200","000000");
+  if( ok ) {
+      logout();
+  }
 }
