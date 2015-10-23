3fef312718645873119b9832a87d8c6e2936b531
jimxim <79202792@qq.com>
2015-10-23 14:53:40 +0800

    1023

--- suite.js
+++ suite.js
@@ -1,26 +1,7 @@
 //JinXinhua <79202792 at qq.com> 20150904
 //测试套具，集合测试用例
 
-#import "lib/common.js"
-#import "lib/sales.js"
-#import "lib/provider.js"
-#import "lib/goods.js"
-#import "lib/param.js"
-
-#import "testcase/wangLai/customer.js"
-#import "testcase/wangLai/customerFields.js"
-#import "testcase/wangLai/provider.js"
-#import "testcase/goods/goodsFields.js"
-#import "testcase/goods/goods.js"
-#import "testcase/system/param.js"
-#import "testcase/system/systemFields.js"
-#import "testcase/sales/salesFields.js"
-#import "testcase/sales/sales.js"
-#import "testcase/statisticAnalysis/statisticAnalysisFields.js"
-#import "testcase/statisticAnalysis/statisticAnalysis.js"
-#import "testcase/statisticAnalysis/saSynthesis.js"
-#import "testcase/purchase/purchaseFields.js"
-#import "testcase/purchase/purchase.js"
+#import "all.js"
 
 // 总经理
 function test000All() {
@@ -48,9 +29,10 @@ function debugElements() {
 }
 
 function testTemp() {
-    testDate();
+//    testDate();
 //    testGetOpTime();
 //    testTapKeyboardHide();
+    test160051();
 }
 
 function testPinyin() {
