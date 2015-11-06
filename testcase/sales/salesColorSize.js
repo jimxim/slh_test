fd22cef45157c8ab6f48cc570c183a53298153a9
stardustlxx <52619481@qq.com>
2015-11-06 17:12:31 +0800

    1106

--- testcase/sales/salesColorSize.js
+++ testcase/sales/salesColorSize.js
@@ -1,55 +1,49 @@
 // luxingxin <52619481 at qq.com> 20151106
 
-function testSalesBillAll() {
-    // run("新增开单，特殊货品", "testSpecial");
-    // run("新增开单，无颜色尺码", "testEditSalesBillNoColorSize");
-    // run("新增开单，颜色尺码", "testEditSalesBillColorSize");
-    // run("开单界面，新增货品", "testEditSalesBillAddGoods");
-    run("开单界面，新增货品,重复明细", "testEditSalesBillDetRepeatGoods");
+//全局参数为颜色尺码+表头尺码
+function testSalesColorSizeAll() {
+//    run("【销售开单】销售开单的同时订货，查看错误提示", "test220008");
+    run("【销售开单】销售开单界面客户适用价格", "test220009");
+
 }
 
-function testSpecial() {
+// 开启参数开单的同时订货
+function test220008() {
     tapMenu("销售开单", "开  单+");
-    var o = { "特殊货品" : { "抹零" : 100, "打包费" : 9 } };
-    editSalesBillSpecial(o);
+    delay();
+    var ret = false;
+    if (isIn(alertMsg, "开启表头尺码模式的开单不能开启开单订货功能")) {
+        tapPrompt();
+        ret = true;
+    }
+    delay();
 
-    return true;
+    return ret;
 }
 
-function testEditSalesBillNoColorSize() {
+function test220009() {
+    var r = "add" + getTimestamp(6);
+    tapMenu("往来管理", "新增客户+");
+    var keys = [ "名称", "适用价格"];
+    var fields = editCustomerFields(keys);
+    changeTFieldValue(fields["名称"], r);
+    setTFieldsValue(getScrollView(), fields);
+    tapButton(window, SAVE);
+    delay();
+    tapButton(window, RETURN);
+    
     tapMenu("销售开单", "开  单+");
-    var json = { "客户" : "zhhz1", "店员" : "005,",
-        "明细" : [ { "货品" : "k526", "数量" : "5" } ], "现金" : "0",
-        "onlytest" : "yes" };
-    json = { "客户" : "zhhz1", "onlytest" : "yes" };
-    editSalesBillNoColorSize(json);
-
-    return true;
-}
-
-function testEditSalesBillColorSize() {
-    // tapMenu("销售开单", "开 单+");
-    var json = {
-        "客户" : "zhhz1",
-        "明细" : [ { "货品" : "14L595", "数量" : [ 5, 6, 7 ] },
-                { "货品" : "150921", "数量" : [ 1, 2, 3 ] } ], "现金" : "0",
-        "核销" : [ 5 ], "特殊货品" : { "抹零" : 100 } };
+    var json = { "客户" : r, 
+               "明细" : [ { "货品" : "3035", "数量" : [ 10 ] } ], "onlytest" : "yes" };
     editSalesBillColorSize(json);
-    return true;
-}
+    
+    
+    
+    
+    
+//    tapMenu("往来管理", "客户查询");
+//    tapFirstText();
 
-function testEditSalesBillAddGoods() {
-    var o = { "款号" : "kh", "名称" : "名称", "进货价" : 120, "零批价" : 130, "打包价" : 140,
-        "大客户价" : 150, "Vip价格" : 160, "添加到单据" : "否" };
-    editSalesBillAddGoods(o);
-}
 
-function testEditSalesBillDetRepeatGoods() {
-    tapMenu("销售开单", "开  单+");
-    var o = { "明细" : [ { "货品" : "ck001", "数量" : "5" },
-            { "货品" : "ck001", "数量" : "5" }, { "货品" : "ck002", "数量" : "5" } ] };
-    o = { "明细" : [ { "货品" : "ck001", "数量" : "5" },
-            { "货品" : "ck002", "数量" : "5" } ] };
-    editSalesBillDetNoColorSize(o);
-    return true;
+    return ret;
 }
