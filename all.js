002fb88a3e5b796f7c73da8756105d9ce747d4ae
jimxim <79202792@qq.com>
2015-11-07 14:16:06 +0800

    lib函数整理

--- all.js
+++ all.js
@@ -4,8 +4,6 @@
 #import "lib/common.js"
 #import "lib/cache.js"
 #import "lib/sales.js"
-#import "lib/provider.js"
-#import "lib/goods.js"
 #import "lib/param.js"
 
 #import "testcase/check/check.js"
