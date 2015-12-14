0974cbaa783955bec037c0470138282b1d858668
2hangying <15068165765@139.com>
2015-12-14 08:54:42 +0800

    suite

--- run/run_suite2.sh
+++ run/run_suite2.sh
@@ -3,6 +3,8 @@ D=`dirname $0`
 
 SUITE=suite2
 js_func="test000SalesNoColorSizeElse001 test000Check001"
+#js_func="$js_func test000SalesNoColorSizeElse001 test000Check001"
+#js_func="$js_func test000SalesNoColorSizeElse001 test000Check001"
 SIMULATOR_NAME_OR_DEVICE_UDID="iPad 2 (8.4)"
 
 . $D/run_uia.sh
