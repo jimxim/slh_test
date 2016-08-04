8a96ee7ed611b2ed1c93898448dd1488e19f6262
stardustlxx <52619481@qq.com>
2016-08-04 10:45:34 +0800

    suite

--- run/run_suite.sh
+++ run/run_suite.sh
@@ -2,7 +2,9 @@
 D=`dirname $0`
 
 SUITE=suite1
-js_func="login200SalesOrderPrepare login000SalesOrder001 login000SalesOrder002"
+js_func="loginGoodsParams001"
+js_func="$js_func loginSASynthesisAll"
+js_func="$js_func login200StatisticAnalysisPrepare login000StatisticAnalysis001"
 SIMULATOR_NAME_OR_DEVICE_UDID="iPad 2 (9.2)"
 
 . $D/run_uia.sh
